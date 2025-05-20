d31c95db5dab5241159c382a73f77d453321b7b2
Dario Romero Delgado <160232957+DarioRodel@users.noreply.github.com>
2025-05-20 14:43:36 +0200

    Update urls.py

--- ControlStock/ControlStock/urls.py
+++ ControlStock/ControlStock/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('control.urls')),
+    path('/', include('control.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
