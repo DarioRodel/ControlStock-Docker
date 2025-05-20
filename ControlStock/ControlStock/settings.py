f2d428c7b9962837a2b71748346cc21ad400efd9
Dario Romero Delgado <160232957+DarioRodel@users.noreply.github.com>
2025-05-20 14:36:11 +0200

    Update settings.py

--- ControlStock/ControlStock/settings.py
+++ ControlStock/ControlStock/settings.py
@@ -133,9 +133,9 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-MEDIA_URL = '/controlstock/media/'
+MEDIA_URL = '/media/'
 FORCE_SCRIPT_NAME = '/'
-STATIC_URL = '/controlstock/static/'
+STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
